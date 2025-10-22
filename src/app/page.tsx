98ebf445982e5afbea1caaabf811680db2b4ec75
puang59 <kk61468@gmail.com>
2025-10-22 17:07:10 +0530

    fix: ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -15,19 +15,15 @@ export default function Home() {
   const words = useQuery(api.words.getWords) || [];
   const [query, setQuery] = useState("");
 
-  const filteredWords = words.filter(word => word.word.toLowerCase().includes(query.toLowerCase()));
+  const filteredWords = words.filter((word) =>
+    word.word.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
-    <main className="flex flex-col items-center justify-center text-black min-h-screen">
-
-      <div className="flex flex-col justify-center items-center mt-5 w-full">
-        <Image
-          src="/logo.png"
-          alt="Lexit Logo"
-          width={100}
-          height={100}
-        />
-        <form className="flex my-10 gap-2 w-[80%]">
+    <main className="flex flex-col items-center justify-center text-black h-screen overflow-hidden">
+      <div className="flex flex-col justify-center items-center mt-5 w-full max-w-3xl mx-auto px-6">
+        <Image src="/logo.png" alt="Lexit Logo" width={100} height={100} />
+        <form className="flex my-10 gap-2 w-full">
           <Input
             type="text"
             placeholder="search words"
@@ -39,7 +35,7 @@ export default function Home() {
       </div>
 
       {/* Words List */}
-      <div className="w-full max-h-[65vh] overflow-y-auto">
+      <div className="w-full max-w-3xl mx-auto max-h-[65vh] overflow-y-auto">
         {filteredWords.length > 0 ? (
           <ul>
             {filteredWords.reverse().map((word) => (
@@ -49,7 +45,6 @@ export default function Home() {
                 meaning={word.meaning}
                 examples={word.examples}
               />
-
             ))}
           </ul>
         ) : (
@@ -57,10 +52,13 @@ export default function Home() {
         )}
       </div>
 
-
       {/* Footer */}
       <footer className="mt-auto mb-10 ml-auto mx-5">
-        <Button size="icon" className="rounded-full" onClick={() => router.push("/add-word")}>
+        <Button
+          size="icon"
+          className="rounded-full"
+          onClick={() => router.push("/add-word")}
+        >
           <Plus size={16} />
         </Button>
       </footer>
